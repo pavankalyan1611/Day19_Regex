66ff2dfb38631b5058b4acfca426a19efff7823e
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-08-04 11:41:08 +0530

    [pavan] Checking lastname using Regex

--- Day19_UserRegistration/Day19_UserRegistration/Program.cs
+++ Day19_UserRegistration/Day19_UserRegistration/Program.cs
@@ -1,12 +1,10 @@
 ﻿using Day19_UserRegistration;
 
 Patterns patterns = new Patterns();
-Console.WriteLine("Enter First Name");
-string firstName = Console.ReadLine();
+Console.WriteLine("\nEnter Last Name");
+string lastName = Console.ReadLine();
 //Console.WriteLine(patterns.validatePinCode("xyz@bridgelabz.com.in.in.in"));
-if (patterns.validateString(firstName))
-{
+if (patterns.validateString(lastName))
     Console.WriteLine("Valid");
-}
 else
     Console.WriteLine("Invalid");
