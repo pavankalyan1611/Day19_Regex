d36b4d35bb2cf631e8ed8207333da508bea61be3
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-08-04 17:49:29 +0530

    [pavan] should have at least One Num

--- Day19_UserRegistration/Day19_UserRegistration/Program.cs
+++ Day19_UserRegistration/Day19_UserRegistration/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Day19_UserRegistration;
 
 Patterns patterns = new Patterns();
-Console.WriteLine("\nEnter ");
+Console.WriteLine("\nEnter Password");
 string rule = Console.ReadLine();
 
 //Console.WriteLine(patterns.validatePinCode("xyz@bridgelabz.com.in.in.in"));
