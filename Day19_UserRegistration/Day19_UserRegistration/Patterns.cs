e73b3f07dce04a162d28c4c4b0cb2909c74ca971
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-08-04 15:51:24 +0530

    [pavan] checking mobile number

--- Day19_UserRegistration/Day19_UserRegistration/Patterns.cs
+++ Day19_UserRegistration/Day19_UserRegistration/Patterns.cs
@@ -9,17 +9,17 @@ namespace Day19_UserRegistration
 {
     public class Patterns
     {
-        public Patterns()                // 
+        public Patterns()                // [.]{8,}
         {
             Console.WriteLine("Welcome to User Registration program using Regular Expressions: ");          
         }
-        public static string rule1 = "[a-z]{8,}";
+        public static string MobileNo =@"^[0-9]{2}\s[0-9]{10}$";  // ^[0-9]{2}[ ][0-9]{10}$
         //public static string Regex_Pincode = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
         //public static string regex = "^[A-Za-z0-9]+([.#_+][A-Za-z0-9]+)*[@][A-Za-z0-9]+[.][a-z]{2,3}([.][a-z]{2})?$";
         //public static string regex = "^[A-Za-z0-9]+([._#%+][A-Za-z0-9]+)?[@][A-Za-z]+[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
         public bool validateString(string sample)
         {
-            return Regex.IsMatch(sample, rule1);
+            return Regex.IsMatch(sample, MobileNo);
         }
     }
 }
--- Day19_UserRegistration/Day19_UserRegistration/Program.cs
+++ Day19_UserRegistration/Day19_UserRegistration/Program.cs
@@ -1,12 +1,12 @@
 ﻿using Day19_UserRegistration;
 
 Patterns patterns = new Patterns();
-Console.WriteLine("\nEnter minimum 8 characters");
-string rule1 = Console.ReadLine();
+Console.WriteLine("\nEnter Country code follow by space and 10 digit number");
+string mobileno = Console.ReadLine();
 
 //Console.WriteLine(patterns.validatePinCode("xyz@bridgelabz.com.in.in.in"));
     
-if (patterns.validateString(rule1))
+if (patterns.validateString(mobileno))
     Console.WriteLine("Valid");
 else
     Console.WriteLine("Invalid");
