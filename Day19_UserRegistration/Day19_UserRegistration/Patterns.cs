f908849c906f508f41ba40c0c70de5f6404e39c7
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-08-04 11:08:41 +0530

    [pavan] Checking firstname using Regex

---
+++ Day19_UserRegistration/Day19_UserRegistration/Patterns.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19_UserRegistration
+{
+    public class Patterns
+    {
+        public Patterns()
+        {
+            Console.WriteLine("Welcome to User Registration program using Regular Expressions: ");          
+        }
+        public static string RegexRules ="^[A-Z][a-z]{3,}";
+        //public static string Regex_Pincode = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+        //public static string regex = "^[A-Za-z0-9]+([.#_+][A-Za-z0-9]+)*[@][A-Za-z0-9]+[.][a-z]{2,3}([.][a-z]{2})?$";
+        //public static string regex = "^[A-Za-z0-9]+([._#%+][A-Za-z0-9]+)?[@][A-Za-z]+[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, RegexRules);
+        }
+    }
+}
---
+++ Day19_UserRegistration/Day19_UserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using Day19_UserRegistration;
+
+Patterns patterns = new Patterns();
+Console.WriteLine("Enter First Name");
+string firstName = Console.ReadLine();
+//Console.WriteLine(patterns.validatePinCode("xyz@bridgelabz.com.in.in.in"));
+if (patterns.validateString(firstName))
+{
+    Console.WriteLine("Valid");
+}
+else
+    Console.WriteLine("Invalid");
