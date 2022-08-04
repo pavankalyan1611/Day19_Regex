18bc70519f1509168ec50c6111629960bf8797de
Pavankalyan1611 <pkkoppisetti@gmail.com>
2022-08-04 15:00:19 +0530

    [pavan] password contains min 8 char

--- Day19_UserRegistration/Day19_UserRegistration/Patterns.cs
+++ Day19_UserRegistration/Day19_UserRegistration/Patterns.cs
@@ -9,17 +9,17 @@ namespace Day19_UserRegistration
 {
     public class Patterns
     {
-        public Patterns()
+        public Patterns()                // 
         {
             Console.WriteLine("Welcome to User Registration program using Regular Expressions: ");          
         }
-        public static string email = "^[a-zA-Z0-9]+([!@#$%^&*()_+.,][a-zA-Z0-9]+)*[@][a-zA-Z]{2,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+        public static string rule1 = "[a-z]{8,}";
         //public static string Regex_Pincode = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
         //public static string regex = "^[A-Za-z0-9]+([.#_+][A-Za-z0-9]+)*[@][A-Za-z0-9]+[.][a-z]{2,3}([.][a-z]{2})?$";
         //public static string regex = "^[A-Za-z0-9]+([._#%+][A-Za-z0-9]+)?[@][A-Za-z]+[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
         public bool validateString(string sample)
         {
-            return Regex.IsMatch(sample, email);
+            return Regex.IsMatch(sample, rule1);
         }
     }
 }
--- Day19_UserRegistration/Day19_UserRegistration/Program.cs
+++ Day19_UserRegistration/Day19_UserRegistration/Program.cs
@@ -1,12 +1,12 @@
 ﻿using Day19_UserRegistration;
 
 Patterns patterns = new Patterns();
-Console.WriteLine("\nEnter Email");
-string email = Console.ReadLine();
+Console.WriteLine("\nEnter minimum 8 characters");
+string rule1 = Console.ReadLine();
 
 //Console.WriteLine(patterns.validatePinCode("xyz@bridgelabz.com.in.in.in"));
     
-if (patterns.validateString(email))
+if (patterns.validateString(rule1))
     Console.WriteLine("Valid");
 else
     Console.WriteLine("Invalid");
