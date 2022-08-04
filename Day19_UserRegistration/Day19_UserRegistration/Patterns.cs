8b691ff0f4b82a0786cbdce6590287828056882e
pavankalyan1611 <105772560+pavankalyan1611@users.noreply.github.com>
2022-08-04 17:17:36 +0530

    Update Patterns.cs

--- Day19_UserRegistration/Day19_UserRegistration/Patterns.cs
+++ Day19_UserRegistration/Day19_UserRegistration/Patterns.cs
@@ -13,7 +13,7 @@ namespace Day19_UserRegistration
         {
             Console.WriteLine("Welcome to User Registration program using Regular Expressions: ");          
         }
-        public static string rule1 = "[a-z]{8,}";
+        public static string rule1 = "[a-zA-Z0-9]{8,}";      // or ".{8,}"  dot represents any one char
         //public static string Regex_Pincode = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
         //public static string regex = "^[A-Za-z0-9]+([.#_+][A-Za-z0-9]+)*[@][A-Za-z0-9]+[.][a-z]{2,3}([.][a-z]{2})?$";
         //public static string regex = "^[A-Za-z0-9]+([._#%+][A-Za-z0-9]+)?[@][A-Za-z]+[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
